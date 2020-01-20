a87995ce49b27367a59de651d291d2473aa51082
EC2 Default User <yamara0709@gmail.com>
2020-01-20 08:43:54 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "新規作成に成功しました。"
       redirect_to @user
-      # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
     end
